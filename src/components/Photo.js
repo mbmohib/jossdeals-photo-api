bb376169e2eb513c9c13d795a3648596b80e2d84
Mohammad Mohibbullah <mbmohib@gmail.com>
2018-10-30 01:34:20 +0600

    Add placeholder image

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
-import Title from './styled/Title';
+import React from 'react'
+import styled from 'styled-components'
+import Title from './styled/Title'
+import placeholderImage from '../images/placeholder.png'
 
 const PhotoWrapper = styled.div`
     position: relative;
-`;
+`
 
 const PhotoImage = styled.img`
     border-radius: 10px;
-`;
+`
 
 const PhotoTitle = styled(Title)`
     width: 96%;
@@ -18,13 +19,37 @@ const PhotoTitle = styled(Title)`
     margin-left: 2%;
     border-radius: 5px;
     background-color: rgba(186, 204, 154, 0.7);
-`;
+`
 
-const Photo = props => (
-    <PhotoWrapper>
-        <PhotoImage src={props.photo.url} alt={props.photo.title}/>
-        <PhotoTitle center size="small">{props.photo.title}</PhotoTitle>
-    </PhotoWrapper>
-)
+class Photo extends React.Component {
+    state = {
+        imageLoaded: false
+    }
 
-export default Photo;
+    handleImageLoad = () => this.setState(() => ({ imageLoaded: true }))
+
+    render() {
+        return (
+            <PhotoWrapper>
+                {/* Show Placeholder image while image loading */}
+                {!this.state.imageLoaded && (
+                    <PhotoImage
+                        src={placeholderImage}
+                        alt={this.props.photo.title}
+                    />
+                )}
+                <PhotoImage
+                    src={this.props.photo.url}
+                    alt={this.props.photo.title}
+                    style={!this.state.imageLoaded ? { visibility: 'hidden' } : {}}
+                    onLoad={this.handleImageLoad}
+                />
+                <PhotoTitle center size="small">
+                    {this.props.photo.title}
+                </PhotoTitle>
+            </PhotoWrapper>
+        )
+    }
+}
+
+export default Photo
