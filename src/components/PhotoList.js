a1bdf1487731d8fbd932e8cf6a9bf80db5826ab4
Mohammad Mohibbullah <mbmohib@gmail.com>
2018-10-29 23:30:16 +0600

    Handle network error

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Fade from '@material-ui/core/Fade';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
 import fetchData from '../api/fetchData';
 import Photo from './Photo';
+import Title from './styled/Title';
 
 const PhotolistWrapper = styled.div`
     background: ${props => props.theme.primaryColor};
@@ -17,11 +20,20 @@ const PhotolistWrapper = styled.div`
     width: 100%;
 `;
 
+const Message = withStyles({
+    root : {
+        padding: '30px',
+        color: '#721c24',
+        backgroundColor: '#f8d7da'
+    }
+})(Paper)
+
 class PhotoList extends React.Component {
 
     state = {
         photos: undefined,
-        searchedPhotos: undefined
+        searchedPhotos: undefined,
+        error: false
     }
 
     componentDidMount() {
@@ -37,16 +49,22 @@ class PhotoList extends React.Component {
 
     async getData() {
         const data = await fetchData('https://jsonplaceholder.typicode.com/photos');
-        const photos = data.data.slice(0, 100);
-
-        this.setState(() => {
-            return {
-                photos 
-            }
-        }, () => {
-            // Run searchPhoto function after recieved data (setState Completed)
-            this.searchPhotos()
-        });
+        
+        try {
+            // Only store first 100 photos
+            const photos = data.data.slice(0, 100);
+    
+            this.setState(() => {
+                return {
+                    photos 
+                }
+            }, () => {
+                // Run searchPhoto function after recieved data (setState Completed)
+                this.searchPhotos()
+            });
+        } catch(error) {
+            this.setState(() => ({ error : true }))
+        }
     }
 
     /**
@@ -66,9 +84,19 @@ class PhotoList extends React.Component {
     render() {
         return (
             <PhotolistWrapper>
+                {/* Show Error message for api request error */}
+                {
+                    this.state.error && 
+                    <Message elevation={2}>
+                        <Title>
+                            Something went wrong! Please reload the page.
+                        </Title>
+                    </Message>
+                }
+
                 {/* Show Loading state while data on the way */}
                 <Fade
-                    in={!this.state.searchedPhotos}
+                    in={!this.state.searchedPhotos && !this.state.error}
                     unmountOnExit
                 >
                     <CircularProgress color="secondary" size={80} thickness={1}/>
